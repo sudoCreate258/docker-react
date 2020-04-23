277aa47f0801e46f4d2eb9cf4f1a6106f2aa32fb
sudocreate258 <sudocreate258@gmail.com>
2020-04-23 11:52:27 -0400

    updated msg

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        What
-        </p>
+        Feature Branch Version
+	</p>
         <a
           className="App-link"
           href="https://reactjs.org"
